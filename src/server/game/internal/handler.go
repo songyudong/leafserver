522434f62556c3a809200c5d3f7e55cdb56fee5b
songyudong <songyd@mail.jj.cn>
2019-01-15 14:33:17 +0800

    test websocket

--- src/server/game/internal/handler.go
+++ src/server/game/internal/handler.go
@@ -1,1 +1,26 @@
 package internal
+
+import(
+	"github.com/name5566/leaf/log"
+	"github.com/name5566/leaf/gate"
+	"reflect"
+	"server/msg"
+)
+
+func init(){
+	handler(&msg.Hello{}, handlerHello)
+}
+
+func handler(m interface{}, h interface{}){
+	skeleton.RegisterChanRPC(reflect.TypeOf(m), h)
+}
+
+func handlerHello(args []interface{}){
+	m := args[0].(*msg.Hello)
+	a := args[1].(gate.Agent)
+
+	log.Debug("hello %v", m.Name)
+	a.WriteMsg(&msg.Hello{
+		Name: "client",
+	})
+}
