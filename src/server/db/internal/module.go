b395d621d7a7963b03e4415983206ee56ac42b6b
songyudong <147894941@qq.com>
2019-01-20 19:09:31 +0800

    db

--- src/server/db/internal/module.go
+++ src/server/db/internal/module.go
@@ -7,6 +7,7 @@ import (
 	"github.com/name5566/leaf/db/mongodb"
 	"github.com/name5566/leaf/log"
 	"github.com/name5566/leaf/module"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var (
@@ -16,6 +17,12 @@ var (
 
 type Module struct {
 	*module.Skeleton
+}
+
+type DBUser struct {
+	UserId   int
+	UserName string
+	Age      int
 }
 
 func (m *Module) OnInit() {
@@ -29,7 +36,53 @@ func (m *Module) OnInit() {
 	}
 	defer c.Close()
 	s := c.Ref()
-	err = s.DB("test").C("counters").RemoveId("test")
+
+	u := DBUser{
+		2234,
+		"song",
+		25,
+	}
+	u1 := DBUser{
+		2235,
+		"guan",
+		26,
+	}
+
+	col := s.DB("banana").C("user")
+	col.DropCollection()
+	col.Insert(&u)
+	col.Insert(&u1)
+
+	update := DBUser{
+		2234,
+		"songyudong",
+		25,
+	}
+	col.Update(bson.M{"userid": 2234}, update)
+	query := col.Find(bson.M{"age": bson.M{"$eq": 25}})
+	us := []DBUser{}
+	query.All(&us)
+	log.Debug("query result %v", us)
+
+	q2 := col.Find(nil)
+	us2 := []DBUser{}
+	q2.All(&us2)
+	log.Debug("query result2 %v", us2)
+
+	u3 := DBUser{}
+	q2.One(&u3)
+	log.Debug("query result3 %v", u3)
+
+	q3 := col.Find(bson.M{"username": "songyudong"}).Select(bson.M{"_id": 1})
+	var r interface{}
+	q3.One(&r)
+	log.Debug("query result4 %v", r)
+	q4 := col.Find(bson.M{"_id": r.(bson.M)["_id"]})
+	u4 := DBUser{}
+	q4.One(&u4)
+	log.Debug("query result5 %v", u4)
+
+	col.Update(bson.M{"username": "songyudong"}, bson.M{"$set": bson.M{"age": 33}})
 }
 
 func (m *Module) OnDestroy() {
