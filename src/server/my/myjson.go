a69233b27f0cef876e88cb3b851842f326a63329
songyudong <songyd@mail.jj.cn>
2019-01-28 09:41:44 +0800

    json

--- src/server/my/myjson.go
+++ src/server/my/myjson.go
@@ -1,19 +1,45 @@
 package mychan
 
 import (
+	"encoding/json"
+
 	"io/ioutil"
-	"os"
 
 	"github.com/name5566/leaf/log"
 )
 
+type ColliderData struct {
+	R []float64
+}
+
+type ColliderRoot struct {
+	Nodes []ColliderData
+}
+
+type JsonStruct struct {
+}
+
 func ExampleJson() {
-	fi, err := os.Open("gamedata/colliderRoot.json")
+
+	JsonParse := &JsonStruct{}
+	v := ColliderRoot{}
+
+	JsonParse.Load("gamedata/colliderRoot.json", &v)
+
+	log.Debug("json string : %v", v)
+}
+
+func (jst *JsonStruct) Load(filename string, v interface{}) {
+	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Debug("not find file")
+		return
 	}
 
-	defer fi.Close()
-	fd, err := ioutil.ReadAll(fi)
-	log.Debug("content : %v", fd)
+	//读取的数据为json格式，需要进行解码
+	err = json.Unmarshal(data, v)
+	if err != nil {
+		log.Debug("unmarshal failed")
+		return
+	}
 }
